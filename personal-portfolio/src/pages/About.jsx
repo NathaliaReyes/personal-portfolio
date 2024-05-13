080e729220eebab9b539f3833fd18202452a597e
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-13 13:41:25 -0600

    Chnaging color of navbar to dark and logo hover to a brighter color

--- personal-portfolio/src/pages/About.jsx
+++ personal-portfolio/src/pages/About.jsx
@@ -7,13 +7,15 @@ export default function About() {
   return (
     <div className="container pt-3">
       <div>
-        <h1>About Me</h1>
+        <h1 className="">About Me</h1>
         <img className="img-silvia" src={image} alt="Image Silvia" />
-        <p className="lead mt-4 text-center">
-          Full stack web developer with background in chemical engineering👩‍🔬,
-          currently in search of internship or junior developer role in order to expand
-          skills and experience in the field. <br /> Strong technical understanding in multiple
-          languages, detail oriented, and ability to learn new concepts quickly.
+        <p className="lead mt-4">
+          Hi! I am Silvia Reyes, a Colombian-born developer currently living in the USA. 
+          After studying chemical engineering for three years <br /> 
+          I became interested in computer science and began coding in 2022. I am particularly
+          interested in developing web apps with polished UI using MERN technologies, and BackEnd programming using databases.
+          In my free time, I enjoy getting outside, salsa dancing, and learning new languages. <br />
+          I am looking forward to connecting with you and continuing my journey as a developer.
         </p>
       </div>
       <div className="container">
