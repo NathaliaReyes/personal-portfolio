072545c836d1bb677a4965b009022f8d716f8065
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-14 09:21:22 -0600

    Rendering the resume section with the pdf and fixing style

--- personal-portfolio/src/pages/Resume.jsx
+++ personal-portfolio/src/pages/Resume.jsx
@@ -1,15 +1,34 @@
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+import { useState } from 'react';
+import cvPdf from '../assets/cv-silvia_reyes.pdf';
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 export default function Resume() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
   }
+
+  return (
+    <div>
+      <h1 className='text-center text-primary mt-3'>Mi Currículum</h1>
+      <div>
+        <Document
+          file={cvPdf}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={1.5} />
+          ))}
+        </Document>
+        <p>Página {pageNumber} de {numPages}</p>
+      </div>
+    </div>
+  );
+}
