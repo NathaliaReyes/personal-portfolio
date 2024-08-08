f38e010471d63a82e7541ac62431c68cdf8d20b9
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-08-08 16:10:35 -0600

    fixing overflow in the portfolio section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,7 +2,7 @@ import Project from "../components/Project";
 
 export default function Portfolio() {
     return (
-      <div className="container pt-3">
+      <div className="pt-3">
         <h1 className="cormorant-infant-bold">My Projects</h1>
         <p className="cormorant-infant-medium">
           Some of my projects are listed below. 
